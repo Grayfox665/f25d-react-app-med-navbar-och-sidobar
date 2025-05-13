313e48e00ae48872589df6acb72b89e2b42a4572
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2025-05-13 23:28:21 +0200

    imported css into navbar component

---
+++ src/Components/NavbarComponents/Navbar.js
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+export default function NavBar() {
+  return (
+    <nav className="Navbar">
+      <h3>Home</h3>
+      <h3>About</h3>
+      <h3>Contact us</h3>
+    </nav>
+  );
+}
